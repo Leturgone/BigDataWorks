e0b66d6353370da04245f276b1ccf1b9ed291a67
Leturgone <finekon221@gmail.com>
2025-09-25 09:45:26 +0300

    added comments

--- work2/work4.py
+++ work2/work4.py
@@ -3,23 +3,27 @@ from sklearn.manifold import TSNE
 from tensorflow.keras.datasets import mnist
 import time
 
-# Загружаем MNIST
+# Загружаем MNIST, база данных образцов рукописного написания цифр
 (x_train, y_train), _ = mnist.load_data()
 X = x_train.reshape((x_train.shape[0], -1))
 labels = y_train
 
+
 # Функция для отображения результатов
 def plot_tsne(embedding, labels, perplexity):
     plt.figure(figsize=(8, 6))
+    # Рисуем точки
     scatter = plt.scatter(embedding[:, 0], embedding[:, 1], c=labels, cmap='tab10', s=10)
     plt.legend(*scatter.legend_elements(), title="Digits")
     plt.title(f't-SNE с perplexity={perplexity}')
     plt.show()
 
+
 # Переменные для хранения времени выполнения
 times = []
 
-# Разные значения perplexity
+# Разные значения perplexity, влияет на то, как алгоритм видит "соседей" точек
+# управляет, насколько крупными будут кластеры
 perplexity_values = [5, 30, 50]
 
 for perplexity in perplexity_values:
