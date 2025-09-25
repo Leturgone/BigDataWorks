c9994f7bf3f766bde6e7a29338ef1d35d97dcd9d
Leturgone <finekon221@gmail.com>
2025-09-25 12:20:13 +0300

    function moved

--- work2/work1.py
+++ work2/work1.py
@@ -1,22 +1,8 @@
 import pandas as pd
 import plotly.graph_objs as go
 
-
-def convert_players(val):
-    if isinstance(val, str):
-        if 'K' in val:
-            number = float(val.replace('K', ''))
-            return number * 1000
-        else:
-            try:
-                return float(val)
-            except:
-                return None
-    else:
-        return val
-
-
 # Задание 1
+
 # Загрузка данных
 data_path = 'games.csv'
 df = pd.read_csv(data_path)
@@ -40,11 +26,26 @@ print(df.isnull().sum())
 
 # Задание 3
 
+
+def convert_players(val):
+    if isinstance(val, str):
+        if 'K' in val:
+            number = float(val.replace('K', ''))
+            return number * 1000
+        else:
+            try:
+                return float(val)
+            except:
+                return None
+    else:
+        return val
+
+
 x_data = df['Title'].loc[0:250]
 y_data = df['Plays'].loc[0:250].apply(convert_players)
 color_data = df['Rating'].loc[0:250].astype(float)
 
-# Создаем бар-чарт
+# Бар-чарт
 fig = go.Figure(data=[go.Bar(
     x=x_data,
     y=y_data,
@@ -55,7 +56,7 @@ fig = go.Figure(data=[go.Bar(
     )
 )])
 
-# Обновляем макет
+# Макет
 fig.update_layout(
     title={
         'text': "Игры, рейтинг и количество игроков",
@@ -66,32 +67,29 @@ fig.update_layout(
         'font': dict(size=20)
     },
     xaxis=dict(
-        title='Название игры',
+        title=dict(text="Название игры", font=dict(size=16)),
         tickangle=315,
         tickfont=dict(size=14),
+        showgrid=True,
         gridwidth=2,
-        gridcolor='ivory'
+        gridcolor="ivory"
     ),
     yaxis=dict(
-        title='Количество игроков',
+        title=dict(text="Количество игроков", font=dict(size=16)),
         tickfont=dict(size=14),
+        showgrid=True,
         gridwidth=2,
-        gridcolor='ivory'
+        gridcolor="ivory"
     ),
-    width=None,  # во всю ширину (по умолчанию)
+    width=None,  # во всю ширину
     height=700,
-    margin=dict(t=50, b=50, l=50, r=50),  # минимальные отступы
+    margin=dict(l=0, r=0, t=50, b=0),  # убираем лишние отступы
     coloraxis=dict(
-        colorscale='Viridis',  # или любой другой
+        colorscale='Viridis',
         colorbar=dict(title='Рейтинг')
     ),
-    plot_bgcolor='white'  # чтобы убрать лишние отступы
+    plot_bgcolor='white'
 )
 
-# Убираем лишние отступы по краям
-fig.update_layout(
-    margin=dict(l=0, r=0, t=50, b=50)
-)
-
-# Показываем график
+# Вывод графика
 fig.show()
