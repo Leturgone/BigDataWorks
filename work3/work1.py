d9a72e2d861ee7014a33149a9a58e9cff6c8a6b4
Leturgone <finekon221@gmail.com>
2025-10-09 00:38:03 +0300

    added task 4

--- work3/work1.py
+++ work3/work1.py
@@ -27,4 +27,35 @@ for i, col in enumerate(numeric_cols):
 plt.tight_layout()
 plt.show()
 
+# Задание 4
 
+cols = ['bmi', 'charges']
+n_cols = len(cols)
+
+fig, axes = plt.subplots(1, n_cols, figsize=(14, 6))
+
+for i, col in enumerate(cols):
+    data = df[col]
+    mean_val = data.mean()
+    median_val = data.median()
+    mode_val = data.mode()[0]
+    std_dev = data.std()
+
+    print(f"\n{col.upper()}:")
+    print(f"Среднее: {mean_val}")
+    print(f"Медиана: {median_val}")
+    print(f"Мода: {mode_val}")
+
+    axes[i].hist(data, bins=20, edgecolor='black', alpha=0.7)
+    axes[i].axvline(mean_val, color='r', linestyle='--', label=f'Среднее = {mean_val:.2f}')
+    axes[i].axvline(median_val, color='g', linestyle='-.', label=f'Медиана = {median_val:.2f}')
+    axes[i].axvline(mode_val, color='purple', linestyle=':', label=f'Мода = {mode_val:.2f}')
+    axes[i].set_title(f'Гистограмма для {col}')
+    axes[i].set_xlabel(col)
+    axes[i].set_ylabel('Частота')
+    axes[i].legend()
+
+plt.tight_layout()
+plt.show()
+
+# Задание 5
