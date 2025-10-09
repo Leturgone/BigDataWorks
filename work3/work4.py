64e8d5ba26a7077dda50ba96ba83867e7d787a5d
Leturgone <finekon221@gmail.com>
2025-10-09 08:53:53 +0300

    added task 15

--- work3/work4.py
+++ work3/work4.py
@@ -7,7 +7,7 @@ observed = np.array([97, 98, 109, 95, 97, 104])
 
 expected = np.array([100, 100, 100, 100, 100, 100])
 
-chi2_stat, p_value = stats.chisquare(f_obs=observed, f_exp=expected)
+hi2_stat, p_value = stats.chisquare(f_obs=observed, f_exp=expected)
 
-print("Статистика χ2 =", chi2_stat)
+print("Статистика χ2 =", hi2_stat)
 print("p-value =", p_value)
---
+++ work3/work5.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+data = pd.DataFrame({
+    'Женат': [89,17,11,43,22,1],
+    'Гражданский брак': [80,22,20,35,6,4],
+    'Не состоит в отношениях': [35,44,35,6,8,22]
+})
+data.index = [
+    'Полный рабочий день',
+    'Частичная занятость',
+    'Временно не работает',
+    'На домохозяйстве',
+    'На пенсии',
+    'Учёба'
+]
+hi2, p, dof, expected = chi2_contingency(data)
+
+print("χ2 =", hi2)
+print("p-value =", p)
+print("Степени свободы =", dof)
+print("Ожидаемые частоты:\n", expected)
